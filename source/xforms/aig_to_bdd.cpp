0092c559fa8f771121e6ddeb59eae12421b14cd4
Bruno Schmitt <bruno@oschmitt.com>
2017-11-13 16:30:41 +0100

    Better verbose info.

--- source/xforms/aig_to_bdd.cpp
+++ source/xforms/aig_to_bdd.cpp
@@ -2,6 +2,7 @@
 | This file is distributed under the BSD 2-Clause License.
 | See LICENSE for details.
 *-----------------------------------------------------------------------------*/
+#include <chrono>
 #include <cstdio>
 #include <vector>
 #include <utility> /* std::pair */
@@ -60,10 +61,10 @@ std::pair<Cudd, std::vector<BDD>> aig_to_bdd(Gia_Man_t *aig, bool reorder, int v
 	Cudd bdd_mngr;
 	std::vector<BDD> nodes(Gia_ManObjNum(aig));
 
+	auto start = std::chrono::high_resolution_clock::now();
 	if (reorder) {
 		bdd_mngr.AutodynEnable();
 	}
-	//bdd_mngr.AutodynEnable(CUDD_REORDER_EXACT);
 	printf("[i] Converting AIG to BDD\n");
 	Gia_ManForEachCiId(aig, id, i) {
 		nodes[id] = bdd_mngr.bddVar(i);
@@ -73,33 +74,41 @@ std::pair<Cudd, std::vector<BDD>> aig_to_bdd(Gia_Man_t *aig, bool reorder, int v
 		const auto c1 = Gia_ObjFaninC0(obj);
 		const auto b2 = nodes[Gia_ObjFaninId1(obj, i)];
 		const auto c2 = Gia_ObjFaninC1(obj);
-		nodes[i] = (c1 ? !b1 : b1) & (c2 ? !b2 : b2);
 		if (verbose) {
-			fprintf(stdout, "\rNode %4d / %4d", i, nodes.size());
+			fprintf(stdout, "\rNode %4d / %4lu", i,
+			        nodes.size() - 1 - Gia_ManCoNum(aig));
 			fflush(stdout);
 		}
+		nodes[i] = (c1 ? !b1 : b1) & (c2 ? !b2 : b2);
 	}
-	//bdd_mngr.ReduceHeap();
-	//bdd_mngr.ReduceHeap(CUDD_REORDER_EXACT);
 	if (verbose) {
-		fprintf(stdout, " Done\n");
+		fprintf(stdout, " [Done]\n");
 	}
 	/* Outputs */
 	std::vector<BDD> outputs;
 	Gia_ManForEachCo(aig, obj, i) {
 		const auto b = nodes[Gia_ObjFaninId0p(aig, obj)];
 		const auto c = Gia_ObjFaninC0(obj);
-		outputs.push_back(c ? !b : b);
-		test(bdd_mngr.getManager(), outputs.back().getNode());
-		fprintf(stdout, "[%d] ", i);
-		if (outputs.back().getNode() == NULL) {
-			fprintf(stdout, "empty DD.\n");
-			continue;
+		if (verbose) {
+			fprintf(stdout, "\rOutput %2d / %2d", i, Gia_ManCoNum(aig) - 1);
+			fflush(stdout);
 		}
-		outputs.back().summary(Gia_ManCiNum(aig));
-		// outputs.back().PrintCover();
+		outputs.push_back(c ? !b : b);
 	}
-	fprintf(stdout, " [Done]\n");
+	if (verbose) {
+		fprintf(stdout, " [Done]\n");
+		std::chrono::duration<double> aig2bdd_time=
+			std::chrono::high_resolution_clock::now() - start;
+
+		printf("[i] Elapsed time: %f\n",  aig2bdd_time.count());
+		for (auto &dd : outputs) {
+			if (dd.getNode() == NULL) {
+				fprintf(stdout, "empty DD.\n");
+				continue;
+			}
+			dd.summary(Gia_ManCiNum(aig));
+		}
+	}
 	if (reorder) {
 		bdd_mngr.AutodynDisable();
 	}
